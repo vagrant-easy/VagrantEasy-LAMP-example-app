71861c6c83ae1d164cdc1f779051850921a76c1a
Jean Lescure <jean@ticowebmedia.com>
2015-03-31 14:57:25 -0600

    Index now shows versions and connection better.

--- index.php
+++ index.php
@@ -3,25 +3,56 @@
 <head>
 	<meta charset="UTF-8">
 	<title>I am an example.</title>
+  <style type="text/css">
+    body{
+      font-family: "Lucida Console", Monaco, monospace;
+      font-size: 20px;
+    }
+    .error{
+      color: #e50000;
+    }
+    .success{
+      color: #33cc00;
+    }
+  </style>
 </head>
 <body>
-	<h1>
-    <?php
-      $servername = "localhost";
-      $username = "example_user";
-      $password = "example_pass";
+	<h1>VagrantEasy Example App</h1>
+  <ul>
+  	<li>
+  		<strong>PHP</strong>
+  		<ul>
+  			<li>Version.......... <?php echo '<span class="success">' . phpversion() . '</span>'; ?></li>
+  		</ul>
+  	</li>
+  	<li>
+  		<strong>MySQL</strong>
+  		<ul>
+  			<li>Version.......... <?php 
+          $servername = "localhost";
+          $username = "example_user";
+          $password = "example_pass";
 
-      try {
-          $conn = new PDO("mysql:host=$servername;dbname=example_db", $username, $password);
-          // set the PDO error mode to exception
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          echo "Connected successfully"; 
+          $link = mysql_connect($servername, $username, $password);
+          if (!$link){ 
+            echo '<span class="error">ERROR</span>';
+          }else{
+            echo '<span class="success">' . mysql_get_server_info() . '</span>';
+            mysql_close($link);
           }
-      catch(PDOException $e)
-          {
-          echo "Connection failed: " . $e->getMessage();
+        ?></li>
+  			<li>Connecting.......... <?php
+          try {
+            $conn = new PDO("mysql:host=$servername;dbname=example_db", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo '<span class="success">OK</span>'; 
+          } catch(PDOException $e) {
+            echo '<span class="error">FAIL: ' . $e->getMessage() . '</span>';
           }
-    ?>
-  </h1>
+        ?></li>
+  		</ul>
+  	</li>
+  </ul>
 </body>
 </html>
